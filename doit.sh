ddcea83a0443e73557538c320d9284f3e73ac43a
Steven Flintham <sgf@lemma.co.uk>
2020-10-26 21:46:11 +0000

    Add more instructions, tweak flag handling

--- doit.sh
+++ doit.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+python make-tables.py > tables.beebasm
 beebasm -w -do z.ssd -v -opt 3 -i em1.beebasm  > em1.lst
--- make-tables.py
+++ make-tables.py
@@ -4,7 +4,7 @@ def mybin(i):
     return ("00000000"+bin(i)[2:])[-8:]
 
 with open("tables.beebasm", "w") as f:
-    parity_bit = 1 << 2
+    parity_bit = 1 << 6
     print("    align &100")
     print(".parity_table")
     for i in range(256):
@@ -13,13 +13,21 @@ with open("tables.beebasm", "w") as f:
 
     print("")
     print("    align &100")
-    print(".flag_permutation_table")
+    print(".z80_to_hybrid_flag_table")
     for i in range(256):
-        # We just need to swap bits 1 and 6
-        j = i & 0b10111101
-        if i & (1 << 1) != 0:
-            j |= 1 << 6
-        if i & (1 << 6) != 0:
-            j |= 1 << 1
+        j  =  i & 0b10010001
+        j |= (i & 0b01000000) >> 5
+        j |= (i & 0b00000100) << 4
+        j |= (i & 0b00000010) << 1
         print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
 
+    print("")
+    print("    align &100")
+    print(".hybrid_to_z80_flag_table")
+    for i in range(256):
+        j  =  i & 0b10010001
+        j |= (i & 0b01000000) >> 4
+        j |= (i & 0b00000100) >> 1
+        j |= (i & 0b00000010) << 5
+        j |=      0b00101000 # unused flag bits are always set
+        print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
