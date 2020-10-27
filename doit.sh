2a4a7a2a947a5985973684f211d27f50c488078f
Steven Flintham <sgf@lemma.co.uk>
2020-10-27 19:49:09 +0000

    Add more instructions

--- doit.sh
+++ doit.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -e
 python make-tables.py > tables.beebasm
 beebasm -w -do z.ssd -v -opt 3 -i em1.beebasm  > em1.lst
--- make-tables.py
+++ make-tables.py
@@ -1,5 +1,12 @@
 from __future__ import print_function
 
+hybrid_flag_s = 1 << 7
+hybrid_flag_p = 1 << 6
+hybrid_flag_h = 1 << 4
+hybrid_flag_n = 1 << 2
+hybrid_flag_z = 1 << 1
+hybrid_flag_c = 1 << 0
+
 def mybin(i):
     return ("00000000"+bin(i)[2:])[-8:]
 
@@ -30,4 +37,34 @@ with open("tables.beebasm", "w") as f:
         j |= (i & 0b00000100) >> 1
         j |= (i & 0b00000010) << 5
         j |=      0b00101000 # unused flag bits are always set
+        print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
+
+    print("")
+    print("    align &100")
+    print(".dec_flag_table")
+    for i in range(256):
+        j = hybrid_flag_n
+        if i == 0:
+            j |= hybrid_flag_z
+        if i >= 0x80:
+            j |= hybrid_flag_s
+        if i == 0x7f:
+            j |= hybrid_flag_p
+        if (i & 8) != ((i + 1) & 8):
+            j |= hybrid_flag_h
+        print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
+
+    print("")
+    print("    align &100")
+    print(".inc_flag_table")
+    for i in range(256):
+        j = 0
+        if i == 0:
+            j |= hybrid_flag_z
+        if i >= 0x80:
+            j |= hybrid_flag_s
+        if i == 0x80:
+            j |= hybrid_flag_p
+        if (i & 16) != ((i + 1) & 16):
+            j |= hybrid_flag_h
         print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
