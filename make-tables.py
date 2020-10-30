85709c64bee6e542ae7a73aa3386af646a939538
Steven Flintham <sgf@lemma.co.uk>
2020-10-30 23:29:16 +0000

    Tidy code, add TODOs
    I was open()-ing a file in make-table.py and then just writing to
    stdout, hence the biggish diff just due to indentation changes.

--- make-tables.py
+++ make-tables.py
@@ -10,61 +10,63 @@ hybrid_flag_c = 1 << 0
 def mybin(i):
     return ("00000000"+bin(i)[2:])[-8:]
 
-with open("tables.beebasm", "w") as f:
-    print("    align &100")
-    print(".parity_table")
-    for i in range(256):
-        parity = (1 + bin(i).count("1")) % 2
-        print("    equb &%02x ; &%02x" % (parity * hybrid_flag_p, i))
+print("; AUTO-GENERATED, DO NOT EDIT! Edit make-tables.py instead.")
+print("")
 
-    print("")
-    print("    align &100")
-    print(".z80_to_hybrid_flag_table")
-    for i in range(256):
-        j  =  i & 0b10010001
-        j |= (i & 0b01000000) >> 5
-        j |= (i & 0b00000100) << 4
-        j |= (i & 0b00000010) << 1
-        print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
+print("    align &100")
+print(".parity_table")
+for i in range(256):
+    parity = (1 + bin(i).count("1")) % 2
+    print("    equb &%02x ; &%02x" % (parity * hybrid_flag_p, i))
 
-    print("")
-    print("    align &100")
-    print(".hybrid_to_z80_flag_table")
-    for i in range(256):
-        j  =  i & 0b10010001
-        j |= (i & 0b01000000) >> 4
-        j |= (i & 0b00000100) >> 1
-        j |= (i & 0b00000010) << 5
-        j |=      0b00101000 # unused flag bits are always set TODO: That's not true, see z80-documented.pdf, but not worrying about it for now
-        print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
+print("")
+print("    align &100")
+print(".z80_to_hybrid_flag_table")
+for i in range(256):
+    j  =  i & 0b10010001
+    j |= (i & 0b01000000) >> 5
+    j |= (i & 0b00000100) << 4
+    j |= (i & 0b00000010) << 1
+    print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
 
-    print("")
-    print("    align &100")
-    print(".dec_flag_table")
-    for i in range(256):
-        j = hybrid_flag_n
-        if i == 0:
-            j |= hybrid_flag_z
-        if i >= 0x80:
-            j |= hybrid_flag_s
-        if i == 0x7f:
-            j |= hybrid_flag_p
-        if (((i + 1) & 8) == 0) and ((i & 8) != 0):
-            j |= hybrid_flag_h
-        print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
+print("")
+print("    align &100")
+print(".hybrid_to_z80_flag_table")
+for i in range(256):
+    j  =  i & 0b10010001
+    j |= (i & 0b01000000) >> 4
+    j |= (i & 0b00000100) >> 1
+    j |= (i & 0b00000010) << 5
+    j |=      0b00101000 # unused flag bits are always set TODO: That's not true, see z80-documented.pdf, but not worrying about it for now
+    print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
 
-    print("")
-    print("    align &100")
-    print(".inc_flag_table")
-    for i in range(256):
-        j = 0
-        if i == 0:
-            j |= hybrid_flag_z
-        if i >= 0x80:
-            j |= hybrid_flag_s
-        if i == 0x80:
-            j |= hybrid_flag_p
-        # SFTODO? if (i & 16) != ((i - 1) & 16):
-        if ((((i - 1) & 0x0f) + 1) & 0x10) != 0:
-            j |= hybrid_flag_h
-        print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
+print("")
+print("    align &100")
+print(".dec_flag_table")
+for i in range(256):
+    j = hybrid_flag_n
+    if i == 0:
+        j |= hybrid_flag_z
+    if i >= 0x80:
+        j |= hybrid_flag_s
+    if i == 0x7f:
+        j |= hybrid_flag_p
+    if (((i + 1) & 8) == 0) and ((i & 8) != 0):
+        j |= hybrid_flag_h
+    print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
+
+print("")
+print("    align &100")
+print(".inc_flag_table")
+for i in range(256):
+    j = 0
+    if i == 0:
+        j |= hybrid_flag_z
+    if i >= 0x80:
+        j |= hybrid_flag_s
+    if i == 0x80:
+        j |= hybrid_flag_p
+    # SFTODO? if (i & 16) != ((i - 1) & 16):
+    if ((((i - 1) & 0x0f) + 1) & 0x10) != 0:
+        j |= hybrid_flag_h
+    print("    equb %%%s ; &%02x %%%s" % (mybin(j), i, mybin(i)))
