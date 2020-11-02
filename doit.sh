4c431b1e935bff9f54e02f69850e5d094f8cd58d
Steven Flintham <sgf@lemma.co.uk>
2020-11-02 21:35:14 +0000

    Build b-em-usable version of test code

--- doit.sh
+++ doit.sh
@@ -3,3 +3,7 @@ set -e
 (cd zexall; ./make.sh)
 python make-tables.py > tables.beebasm
 beebasm -w -do z.ssd -v -opt 3 -i em1.beebasm  > em1.lst
+cp cpm-blank.dsd testcpm.dsd
+python ~/Dropbox/cpm-tmpish/deinterleave.py
+cpmcp -f acorn3a testcpm.dsk zexall/zexdoc-sf.cim 0:zexdoc.com
+python ~/Dropbox/cpm-tmpish/interleave.py
